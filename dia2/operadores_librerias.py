e3430186ef9ed80b8dc1ae0ba2f545f96a486bda
Israel Palma <ipalm001@gmail.com>
2024-03-13 13:02:31 -0300

    tipos de datos 2

---
+++ dia2/operadores_librerias.py
@@ -0,0 +1,14 @@
+#import math
+from math import pow,sqrt,ceil
+
+print(2**4)#exponenciacion o potencia o elevado a #16
+print(5/2)#division con resultado flotante; 2.5
+print(5//2)#division con resultado solo la parte entera; 2
+print(5%2)#modulo o resto de la division; 1
+
+#print(math.pow(4,2))
+#print(math.sqrt(100))
+
+print(pow(4,2))
+print(sqrt(100))
+print(ceil(3.001))#aproxima al entero superior
