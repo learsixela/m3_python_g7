9e91c551e9abb0b1f1500dc97b8d249fcd587c74
Israel Palma <ipalm001@gmail.com>
2024-03-26 09:49:10 -0300

    Dia 10: Estructuras de datos Diccionario I agregar/modificar/eliminar

--- dia10/diccionarios.py
+++ dia10/diccionarios.py
@@ -6,6 +6,8 @@ se accede a los elementos a traves de la clave, no importa la posición
 las claves no se generan automaticamente, no hay un orden
 las claves pueden ser String, numerico, incluso Boolean
 los valores pueden ser: String, numerico, boolean, listas, diccionario u otro tipo de datos
+Si la clave existe,modifico su valor, si no existe se agrega el par clave:valor
+
 """
 from os import system
 diccionario = {
@@ -26,7 +28,7 @@ notas = {
     "Daniela": 5,
     "Vicente": 7,
     "FELIPE": 2,
-    "Vicente": 1,
+    "Vicente": None,
 }
 system("clear")
 print(len(notas))#
@@ -39,3 +41,37 @@ print(notas["Felipe"]) #6
 print(notas["Daniela"])#5
 print(notas["Vicente"])#1 reemplaza a 7
 #print(notas["felipe"])#KeyError: 'felipe'
+
+print(notas)#{'Camila': 7, 'Antonio': 5, 'Felipe': 6, 'Daniela': 5, 'Vicente': 1, 'FELIPE': 2}
+notas["Julio"] = 4
+print(notas)#{'Camila': 7, 'Antonio': 5, 'Felipe': 6, 'Daniela': 5, 'Vicente': 1, 'FELIPE': 2, 'Julio': 4}
+
+#julio = {"nota1":7,"nota2":4}
+
+#Modificar par clave:valor
+notas["Julio"] = 5
+print(notas)#
+
+#eliminar par clave:valor
+del notas["FELIPE"]
+print(notas)#{'Camila': 7, 'Antonio': 5, 'Felipe': 6, 'Daniela': 5, 'Vicente': 1, 'Julio': 5}
+
+#2 forma pop()-> al eliminar, capturamos el valor
+eliminado = notas.pop("Camila") #notas["Camila"]
+print("valor eliminado : ",eliminado)#7
+print(notas)#{'Antonio': 5, 'Felipe': 6, 'Daniela': 5, 'Vicente': 1, 'Julio': 5}
+print("")
+notas2 = {
+    "Mijail":2,
+    "Israel":1,
+    "Felipe": 7,
+    }
+
+#notas.update(notas2)
+#print(notas)#
+notas2.update(notas)
+print(notas2)#
+#COLISIONES: al existir duplicidad de claves, se conserva el valor del dicionario anexado
+
+
+
